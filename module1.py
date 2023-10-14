00166f73dd5715a53ba6ec36b9d70f4b09ca1254
jdwesley <136734120+jdwesley@users.noreply.github.com>
2023-10-13 23:11:00 -0600

    Add files via upload

---
+++ module1.py
@@ -0,0 +1,38 @@
+#Addition
+
+num1 = int(input("Chose number 1 :"))
+num2 = int(input("Chose number 2 :"))
+
+result = num1 + num2
+
+print("The sum is :",num1," + ",num2," = ",result)
+
+#Subtraction
+
+num1 = int(input("Chose number 1 :"))
+num2 = int(input("Chose number 2 :"))
+
+result = num1 - num2
+
+print("The difference is :",num1," - ",num2," = ",result)
+
+#Multiplication
+
+num1 = int(input("Chose number 1 :"))
+num2 = int(input("Chose number 2 :"))
+
+result = num1 * num2
+
+print("The product is :",num1," x ",num2," = ",result)
+
+#Division
+
+num1 = int(input("Chose number 1 :"))
+num2 = int(input("Chose number 2 :"))
+
+result = num1 / num2
+
+print("The quotient is :",num1," x ",num2," = ",result)
+
+
+
